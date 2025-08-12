a0a99da96511d40bd18b11adc530c85c6a706ac1
João Victor Prodocimo <jvlprodocimo@gmail.com>
2025-08-11 21:50:05 -0300

    animated menu

--- assets/js/menu-mobile.js
+++ assets/js/menu-mobile.js
@@ -1,14 +1,23 @@
 const MenuBtn = document.getElementsByClassName("menu-btn")[0];
 const MenuModal = document.getElementsByClassName("nav-modal")[0];
 const Menu = document.getElementsByClassName("nav-mobile")[0];
+const MenuDeg1 = document.getElementsByClassName("deg-1")[0];
+const MenuDeg2 = document.getElementsByClassName("deg-2")[0];
+const MenuCute = document.getElementsByClassName("cute")[0];
 
 function OpenMenu() {
     if(MenuModal.classList.contains("toggle")) {
         MenuModal.classList.remove("toggle");
         Menu.classList.remove("toggle");
+        MenuDeg1.classList.remove("toggle");
+        MenuDeg2.classList.remove("toggle");
+        MenuCute.classList.remove("toggle");
     } else {
         MenuModal.classList.add("toggle");
         Menu.classList.add("toggle")
+        MenuDeg1.classList.add("toggle");
+        MenuDeg2.classList.add("toggle");
+        MenuCute.classList.add("toggle");
     }
 }
 
